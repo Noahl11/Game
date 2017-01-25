9981652dc38d915444044c6addcaad6e2d463d39
Noahl11 <noahmznri@gmail.com>
2017-01-24 23:20:48 -0500

    WindowDone0.0.1

--- Game/Game/Window.cpp
+++ Game/Game/Window.cpp
@@ -33,4 +33,24 @@ int Window::create(std::string windowName, int screenWidth, int screenHeight, un
 	if (glContext == nullptr) {
 		//Error Text
 	}
+
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		//Error Text
+	}
+
+	std::printf("*** OpenGL Version: %s ***\n", glGetString(GL_VERSION));
+
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+	SDL_GL_SetSwapInterval(1);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	return 0;
+}
+
+void Window::swapBuffer() {
+	SDL_GL_SwapWindow(m_sdlWindow);
 }
