e83c7fb100a78233acc5a2201696c5c0776793e4
Noahl11 <noahmznri@gmail.com>
2017-01-25 22:50:48 -0500

    Camera
    Update 1

--- Game/Game/Camera.cpp
+++ Game/Game/Camera.cpp
@@ -1,11 +1,26 @@
 #include "Camera.h"
 
 
-Camera::Camera()
+Camera::Camera() : m_position(0.0f, 0.0f, 0.0f), m_cameraMatrix(1.0f), m_persMatrix(1.0f), 
+					m_needsMatrixUpdate(true), m_screenWidth(500), m_screenHeight(500)
 {
 }
 
 
 Camera::~Camera()
 {
+}
+
+void Camera::init(int screenWidth, int screenHeight) {
+	m_screenWidth = m_screenWidth;
+	m_screenHeight = m_screenHeight;
+	m_persMatrix = glm::perspective(glm::radians(45.0f), (float)m_screenWidth / (float)m_screenHeight, 0.1f, 100.0f);
+}
+
+void Camera::update() {
+
+	if (m_needsMatrixUpdate) {
+		glm::vec3 translation();
+	}
+
 }
--- Game/Game/Camera.h
+++ Game/Game/Camera.h
@@ -13,10 +13,19 @@ public:
 
 	void update();
 
-	glm::vec2 convertScreenToWorld(glm::vec2 screenCoords);
+	glm::vec3 convertScreenToWorld(glm::vec3 screenCoords);
 
 	//bool isBoxInView(const glm::vec2& position, const glm::vec2 dimentions);
 
+	void setPosition(const glm::vec3& newPosition) { m_position = newPosition; m_needsMatrixUpdate = true; }
 
+	glm::vec3 getPosition() { return m_position; }
+	glm::mat4 getCameraMatrix() { return m_cameraMatrix; }
+private:
+	int m_screenWidth, m_screenHeight;
+	bool m_needsMatrixUpdate;
+	glm::vec3 m_position;
+	glm::mat4 m_cameraMatrix;
+	glm::mat4 m_persMatrix;
 };
 
