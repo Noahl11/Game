2273307f5700fb63672e64b33fc61724a9ed71a5
Noahl11 <noahmznri@gmail.com>
2017-01-28 21:03:19 -0500

    Gravity

--- Game/Game/SoildPhysics.cpp
+++ Game/Game/SoildPhysics.cpp
@@ -10,7 +10,11 @@ SoildPhysics::~SoildPhysics()
 {
 }
 glm::vec3 SoildPhysics::Velocity() {
-    glm::vec3 answer -32x + I
+    //Do some maths here
     return answer;
 }
 
+float SoildPhysics::Gravity(float mass1, float mass2, float distance) {
+    newtons = (6.67408 × 10-11)*((mass1* mass2)/distance^2);
+    return newtons;   
+}
